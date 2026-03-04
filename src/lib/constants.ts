8addf8fd32db0bc945031e850de1ec113c7b9a2c
jarl9804 <ingandres89@gmail.com>
2026-03-04 18:01:13 +0100

    fix: bump DB_NAME to V3 to force SW cache invalidation + new JS hash

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,9 @@
 export const GOOGLE_SCRIPT_URL =
   'https://script.google.com/macros/s/AKfycbz6YI1Oh-tutU3q5NfPJxDq77QKDMVX6DtM92YZ_GxgKYqm0XXymVCOi08k4SuDteXr/exec'
 
-export const DB_NAME = 'FieldReportV2'
-export const DB_VERSION = 1
+export const DB_NAME = 'FieldReportV3'
+export const DB_VERSION = 2
+export const APP_VERSION = '2026.03.04'
 export const DB_STORE = 'submissions'
 
 export const BLUR_THRESHOLD = 100
